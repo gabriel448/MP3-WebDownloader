dc14c68963cc972670c763dfb245bbe69a12474a
Gabriel Itaborai <gabrieldesousa008@gmail.com>
2025-09-07 18:40:43 -0300

    agora salva o task_id na sessao do usuario

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from flask import Flask,render_template,request,send_from_directory, flash, url_for, redirect
+from flask import Flask,render_template,request,send_from_directory, flash, url_for, redirect,session
 from funcoes import url_verify
 from tasks import mp3_downloader, playlist_downloader
 
@@ -31,21 +31,26 @@ def pagina_inicial():
         #verifica se a url eh valida
         if url_verify(url):
             flash('URL invalida! Por favor, insira uma URL valida do YouTube')
-            return render_template('index.html')
+            return redirect(url_for('pagina_inicial'))
 
         #faz o download conforme o tipo desejado
         #o .delay eh uma funcionalidade do celary, isso manda a tarefa pra "lista de tarefas" no Redis e assim 
         #o download fica em segundo plano e a tela nao congela enquanto faz o download
         if tipo_download == 'video':
-            nome_arquivo = mp3_downloader.delay(url, DOWNLOAD_FOLDER)
+            #pegamos o retorno do delay em uma variavel task
+            task = mp3_downloader.delay(url, DOWNLOAD_FOLDER)
         else:
-            nome_pasta = playlist_downloader.delay(url, DOWNLOAD_FOLDER)
+            task = playlist_downloader.delay(url, DOWNLOAD_FOLDER)
 
-        flash('seu donwload foi iniciado e logo sera salvo no servidor')
+        #setamos o id da task na sessao do usuario 
+        session['task_id'] = task.id
 
         return redirect(url_for('pagina_inicial'))
-    #se nao ele so continua na pagina inicial
-    return render_template('index.html')
+    
+    #pegamos o id da sessao do cliente se existir(se n so retorna none e ta suave)
+    task_id = session.get('task_id')
+    #entao ele retorna a pagina inicial com o id da sessao
+    return render_template('index.html', task_id=task_id)
 
 #se tiver na main ele carrega a aplicacao (caso alguma funcao daqui for chamada em outro arquivo ele nao carrega tudo dnv)
 if __name__ == '__main__':
