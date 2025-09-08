1e224d5d27c212fe6aa7ad24488c03d706e1af2f
Gabriel Itaborai <gabrieldesousa008@gmail.com>
2025-09-07 23:39:24 -0300

    adicao de uma rota para downloads e correcao de bugs em relacao a task_id

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import shutil
-from flask import Flask,render_template,request,send_from_directory, flash, url_for, redirect,session,jsonify
+import tempfile
+from flask import Flask,render_template,request,send_from_directory, flash, url_for, redirect,session,jsonify, send_file
 from funcoes import url_verify
 from tasks import mp3_downloader, playlist_downloader
 from celery.result import AsyncResult
@@ -44,36 +45,40 @@ def pagina_inicial():
         else:
             task = playlist_downloader.delay(url, DOWNLOAD_FOLDER)
 
-        #setamos o id da task na sessao do usuario 
-        session['task_id'] = task.id
-
-        return redirect(url_for('pagina_inicial'))
+        #retornamos a pagina tarefa com a task_id criada para monitorar o status do download
+        return redirect(url_for('pagina_tarefa', task_id=task.id))
     
-    #pegamos o id da sessao do cliente se existir(se n so retorna none e ta suave)
-    task_id = session.get('task_id')
-    #entao ele retorna a pagina inicial com o id da sessao
+    #retornamos o index.html em caso de get com a task_id como None
+    return render_template('index.html', task_id=None)
+
+#uma pagina tarefas exatamente igual a inicial porem com task_id, isso faz com que toda vez que a pagina inicial for chamada
+#com GET a task_id eh pagada, iss evita o navegador ficar mostrando "Download concluido" depois que voce sai e entra na pagina
+#assim sempre que abrimos o site temos a pagina limpa e etc..
+@app.route('/task/<task_id>')
+def pagina_tarefa(task_id):
     return render_template('index.html', task_id=task_id)
 
 #nova rota pra verificar o status do download
 @app.route('/status/<task_id>')
 def task_status(task_id):
+
     #objeto do celery que tem todas a informacoes de uma task especifica atraves do seu id
     task = AsyncResult(task_id, app=celery_app)
 
     #verifica se esta pendente, em processamento, finalizado, ou se ocorreu um erro
     #gerando um dicionario com o stado(mais formal e oficial) e o status(oque significa na pratica)
-    if task.state == 'PENDING':
-        response = {
-            'state': task.state,
-            'status': 'Na fila...'
-        }
-    elif task.state != 'FAILURE':
+
+    if task.state != 'FAILURE':
         response = {
             'state': task.state,
             'status': 'Processando...'
         }
         if task.state == 'SUCCESS':
             #aqui ele retorna o resultado que no caso eh o nome do arquivo mp3 que baixamos
+            response = {
+                'state': task.state,
+                'status': 'Download conlcuido!!'
+            }
             response['result'] = task.result
     else:
         response = {
@@ -83,6 +88,30 @@ def task_status(task_id):
 
     #retorna o dicionario em um JSON para o JS analisar e decidir oque fazer dependendo da situacao do download
     return jsonify(response)
+
+@app.route('/downloads/<string:type>/<path:name>')
+def download(type,name):
+    if type == 'arquivo':
+        #so manda o arquivo direto pro downloads do usuario
+        return send_from_directory(DOWNLOAD_FOLDER,name, as_attachment = True)
+    
+    elif type == 'playlist':
+        #pega o diretorio onde estao baixados os mp3s da playlist
+        playlist_path = os.path.join(DOWNLOAD_FOLDER, name)
+
+        #cria um nome temporario pro zip temporario (tmp.name retorna o diretorio exato desse zip temporario)
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.zip')as tmp:
+            zip_path = tmp.name
+        
+        #cria de fato o arquivo zip com todos os mp3s
+        shutil.make_archive(zip_path.replace('.zip',''),'zip',playlist_path)
+
+        #envia o zip pra download do usuario
+        return send_file(zip_path, as_attachment=True, download_name=f'{name}.zip')
+    
+    else:
+        return "tipo de download invalido", 400
+
 
 #se tiver na main ele carrega a aplicacao (caso alguma funcao daqui for chamada em outro arquivo ele nao carrega tudo dnv)
 if __name__ == '__main__':
